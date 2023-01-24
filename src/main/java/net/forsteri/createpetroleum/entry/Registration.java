ac4851abcbae3ea7eb803f00ef80430426bfc095
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2023-01-24 12:01:45 +0800

    feat: working distillation tank without any distillation recipe

--- src/main/java/net/forsteri/createpetroleum/entry/Registration.java
+++ src/main/java/net/forsteri/createpetroleum/entry/Registration.java
@@ -1,8 +1,8 @@
 package net.forsteri.createpetroleum.entry;
 
-import com.simibubi.create.AllBlocks;
-import com.simibubi.create.content.contraptions.fluids.tank.*;
-import com.simibubi.create.content.logistics.block.display.source.BoilerDisplaySource;
+import com.simibubi.create.content.contraptions.fluids.tank.FluidTankGenerator;
+import com.simibubi.create.content.contraptions.fluids.tank.FluidTankModel;
+import com.simibubi.create.content.contraptions.fluids.tank.FluidTankRenderer;
 import com.simibubi.create.foundation.data.AssetLookup;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.data.SharedProperties;
@@ -10,14 +10,11 @@ import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.forsteri.createpetroleum.CreatePetroleum;
 import net.forsteri.createpetroleum.content.DistillationTankBlock;
-import net.forsteri.createpetroleum.content.DistillationTankGenerator;
-import net.forsteri.createpetroleum.content.DistillationTankRenderer;
+import net.forsteri.createpetroleum.content.DistillationTankItem;
 import net.forsteri.createpetroleum.content.DistillationTankTileEntity;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
-import static com.simibubi.create.Create.REGISTRATE;
-import static com.simibubi.create.content.logistics.block.display.AllDisplayBehaviours.assignDataBehaviour;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 
 public class Registration {
@@ -27,20 +24,19 @@ public class Registration {
     public static final BlockEntry<DistillationTankBlock> DISTILLATION_TANK_BLOCK = REGISTRATE.block("distillation_tank", DistillationTankBlock::new)
             .initialProperties(SharedProperties::copperMetal)
             .properties(BlockBehaviour.Properties::noOcclusion)
-            .properties(p -> p.isRedstoneConductor((p1, p2, p3) -> true))
             .transform(pickaxeOnly())
-            .blockstate(new DistillationTankGenerator()::generate)
+            .blockstate(new FluidTankGenerator()::generate)
             .onRegister(CreateRegistrate.blockModel(() -> FluidTankModel::standard))
             .addLayer(() -> RenderType::cutoutMipped)
-            .item(FluidTankItem::new)
-            .model(AssetLookup.<FluidTankItem>customBlockItemModel("_", "block_single_window"))
+            .item(DistillationTankItem::new)
+            .model(AssetLookup.customBlockItemModel("_", "block_single_window"))
             .build()
             .register();
 
     public static final BlockEntityEntry<DistillationTankTileEntity> DISTILLATION_TANK_TILE_ENTITY = REGISTRATE
             .tileEntity("distillation_tank", DistillationTankTileEntity::new)
-            .validBlocks(AllBlocks.FLUID_TANK)
-            .renderer(() -> DistillationTankRenderer::new)
+            .validBlocks(Registration.DISTILLATION_TANK_BLOCK)
+            .renderer(() -> FluidTankRenderer::new)
             .register();
 
     public static void register() {}
